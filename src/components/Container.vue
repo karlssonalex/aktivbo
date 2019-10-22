016abf973351bfe989319149c3d7bb44989f0207
Alex Karlsson <karlsson.alex92@gmail.com>
2019-10-23 00:05:50 +0200

    Finish test

---
+++ src/components/Container.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>AktivBo List</h1>
+
+        <input v-model="search" class="form-control" placeholder="Filter by survey type">
+
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th v-for="(value, column) in columns" v-bind:key="column">
+                <a href="#" @click="changeSort(column)">
+                  {{ value }}
+                </a>
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="data in orderedList" v-bind:key="data.project_id">
+              <td>{{ data.project_status }}</td>
+              <td>{{ data.survey_type }}</td>
+              <td>{{ data.project_name }}</td>
+              <td>{{ data.respondent_count }}</td>
+              <td>{{ data.date_start }}</td>
+              <td>{{ data.date_end }}</td>
+              <td>{{ data.integration }}</td>
+              <td>{{ data.answer_rate_latest }}</td>
+              <td>&#11088;</td>
+            </tr>
+          </tbody>
+
+        </table>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from '../json/aktivbo.json'
+import _ from 'lodash'
+
+export default {
+  name: 'Container',
+
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+  },
+  computed: {
+    orderedList: function () {
+      var search = this.search
+      var filteredList =  _.filter(this.jsonData, function(o) {
+        if(o.survey_type.toLowerCase().includes(search.toLowerCase())){
+          return o.survey_type
+        }
+      })
+      return _.orderBy(filteredList, this.sortBy, this.descAsc)
+    }
+  },
+
+  data: function() {
+    return {
+      sortBy:"",
+      descAsc: 'asc',
+      order: true,
+      search: '',
+      columns: {
+          project_status : 'Project Status',
+          survey_type : "Survey Type",
+          project_name : "Project Name",
+          respondent_count : "Respondent Count",
+          date_start : "Start Date",
+          date_end : "End Date",
+          integration : "Integration",
+          answer_rate_latest : "Answer Rate"
+      },
+      jsonData: json.data.listSurveys
+
+    };
+  },
+
+  methods: {
+    changeSort: function(newSort) {
+      this.sortBy = newSort
+      this.order = !this.order;
+      this.descAsc = this.order ? 'desc' : 'asc';
+    },
+    addFavorite: function() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+td {
+  text-align: left;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col">
+      {{data}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ListItem',
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
